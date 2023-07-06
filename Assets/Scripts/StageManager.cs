353c5d89ec44b9cbe794f69a33cb842e6c543fdc
jjjiiihhhooo <thdwlgh950@naver.com>
2023-07-07 06:40:20 +0900

    폴더, 스크립트 설정(2023.07.07)

---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour, Interface
+{
+
+
+    public void Enter()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
+    
