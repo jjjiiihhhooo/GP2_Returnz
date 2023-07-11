40fafb60e2dd59f67920656c747b1b367d4cbb54
jjjiiihhhooo <thdwlgh950@naver.com>
2023-07-12 00:06:40 +0900

    임시

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -7,3 +7,14 @@ interface Interface
     void Enter();
     void Exit();
 }
+
+interface IState<T>
+{
+    public void StateChange(T Controller);
+
+    public void StateEnter(T Controller);
+
+    public void StateUpdate(T Controller);
+
+    public void StateExit(T Controller);
+}
